91f2aeea0c3f17cbeab39d89c6fc7ae14c1e5642
m.lutin <mickael.lutin@fluedis.com>
2019-03-21 18:09:09 +0100

    modification portfolio

--- www/portfolio.php
+++ www/portfolio.php
@@ -50,39 +50,43 @@ include ('admin/includes/config.php');
                         </div>
                     </div>
                 </div>
-            </div><br><br><br><br><br><br><br><br><br><br><br><br>';
+            </div>';
 
 
                     echo '<!-- Full-width images with number text -->
-<div align="right">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="container-fluid">
+<div align="center">
   <div class="mySlides">
     <div class="numbertext">1 / 6</div>
-      <img src="venum.png" style="width:10%">
+      <img src="venum.png" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">2 / 6</div>
-      <img src="venum.png" style="width:10%">
+      <img src="venum.png" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">3 / 6</div>
-      <img src="venum.png" style="width:10%">
+      <img src="venum.png" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">4 / 6</div>
-      <img src="venum.png" style="width:10%">
+      <img src="venum.png" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">5 / 6</div>
-      <img src="venum.png" style="width:10%">
+      <img src="venum.png" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">6 / 6</div>
-      <img src="venum.png" style="width:10%">
+      <img src="venum.png" style="width:50%">
   </div>
 </div>
 
@@ -98,24 +102,28 @@ include ('admin/includes/config.php');
   <!-- Thumbnail images -->
   <div class="row">
     <div class="column">
-      <img class="demo cursor" src="venum.png" style="width:50%" onclick="currentSlide(1)" alt="The Woods">
+      <img class="demo cursor" src="venum.png" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
     </div>
     <div class="column"> 
-      <img class="demo cursor" src="venum.png" style="width:50%" onclick="currentSlide(2)" alt="Cinque Terre">
+      <img class="demo cursor" src="venum.png" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
     </div>
     <div class="column">
-      <img class="demo cursor" src="venum.png" style="width:50%" onclick="currentSlide(3)" alt="Mountains and fjords">
+      <img class="demo cursor" src="venum.png" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
     </div>
     <div class="column">
-      <img class="demo cursor" src="venum.png" style="width:50%" onclick="currentSlide(4)" alt="Northern Lights">
+      <img class="demo cursor" src="venum.png" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
     </div>
     <div class="column">
-      <img class="demo cursor" src="venum.png" style="width:50%" onclick="currentSlide(5)" alt="Nature and sunrise">
+      <img class="demo cursor" src="venum.png" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
     </div> 
     <div class="column">
-      <img class="demo cursor" src="venum.png" style="width:50%" onclick="currentSlide(6)" alt="Snowy Mountains">
+      <img class="demo cursor" src="venum.png" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
     </div>
-  </div>';
+  </div>
+                              </div>
+                        </div>
+                    </div>
+                </div>';
                 }
             }
         }
