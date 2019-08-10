dcc3588665c57f078c2b587e4c936ff0f2fa1d2d
Mickael Lutin <lutmickael@gmail.com>
2019-08-10 23:04:46 +0200

    Create config.php

---
+++ www/admin/includes/config.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+try {
+    $db = new PDO('mysql:host=localhost;dbname=lucilledrx928', 'root', 'root');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+define('SITE_URL', 'http://localhost/www');
+//define('SITE_URL', 'http://lucillede.com');
+
+?>
+
